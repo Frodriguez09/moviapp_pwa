4e348e15c812eb76f5f59edd63bd864ad4caa57d
Frodriguez09 <37087731+Frodriguez09@users.noreply.github.com>
2023-03-15 23:44:57 -0700

    app de movies pwa

---
+++ src/Pages/About/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import LanguageIcon from "@material-ui/icons/Language";
+import ArtTrackIcon from "@material-ui/icons/ArtTrack";
+import Tooltip from "@material-ui/core/Tooltip";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className='about'>
+      
+      <div className='social'>
+        
+      </div>
+      <div className='description'>
+        
+      </div>
+    </div>
+  );
+};
+
+export default About;
